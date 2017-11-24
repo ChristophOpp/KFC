3484d78230fddeb4d6be989089fb2fc9a2c3345e
Christoph Opperer <christoph.opperer@hak-kitz.ac.at>
2017-11-24 08:31:49 +0100

    1.Commit

--- KitzFriendsClub(KFC)/Form1.cs
+++ KitzFriendsClub(KFC)/Form1.cs
@@ -39,14 +39,17 @@ namespace KitzFriendsClub_KFC_
         {
             if (rbMitarbeiter.Checked == true)
             {
+                //New Connection + SQLCommand
                 MySqlConnection con = new MySqlConnection("server = eduweb.kb.local; database = team10; username = team10; password = T3amO10");
                 MySqlCommand com = new MySqlCommand("SELECT COUNT(*) as cnt from Mitarbeiter where username = @usr and password = @pwd", con);
                 com.Parameters.Clear();
+                //Die eingegebenen Variablen werden als "username" und "password" verwendet.
                 com.Parameters.AddWithValue("@usr", txt_username.Text);
                 com.Parameters.AddWithValue("@pwd", txt_password.Text);
                 con.Open();
 
                 //MessageBox.Show(com.ExecuteScalar().ToString());
+                //wenn es einen Dateieintrag gibt, bei dem "username" und "password" übereinstimmen wird weitergeleitet.
                 if (com.ExecuteScalar().ToString()=="1")
                 {
                     //MessageBox.Show("Passt!");
@@ -61,14 +64,17 @@ namespace KitzFriendsClub_KFC_
             }
             else if (rbCEO.Checked = true)
             {
+                //New Connection + SQLCommand
                 MySqlConnection con = new MySqlConnection("server = eduweb.kb.local; database = team10; username = team10; password = T3amO10");
                 MySqlCommand com = new MySqlCommand("SELECT COUNT(*) as cnt from CEO where username = @usr and password = @pwd", con);
                 com.Parameters.Clear();
+                //Die eingegebenen Variablen werden als "username" und "password" verwendet.
                 com.Parameters.AddWithValue("@usr", txt_username.Text);
                 com.Parameters.AddWithValue("@pwd", txt_password.Text);
                 con.Open();
 
                 //MessageBox.Show(com.ExecuteScalar().ToString());
+                //wenn es einen Dateieintrag gibt, bei dem "username" und "password" übereinstimmen wird weitergeleitet.
                 if (com.ExecuteScalar().ToString() == "1")
                 {
                     //MessageBox.Show("Passt!");
@@ -77,6 +83,7 @@ namespace KitzFriendsClub_KFC_
                     Hide();
                 }
                 else
+                //wenn "password" oder/und "username" nicht stimmen, wird der Zugang verweigert und es wird eine Fehlermeldung ausgegeben.
                 {
                     MessageBox.Show("Nein");
                 }
